3a70ca7bed79230ddbf04ba5d521b3be013cf107
Manoel Vilela <manoel.neto@neoway.com.br>
2025-08-24 22:09:08 -0300

    feat: improve src/tree/main.c tester with command aliases
    >>> i 10
    >>> i 20
    >>> i 30
    >>> p
    
    For fast iteration

--- src/tree/main.c
+++ src/tree/main.c
@@ -23,7 +23,7 @@
  */
 
 void help() {
-	printf("usage: [insert num | remove num | infix | posfix | prefix | print | clear | help | exit]\n");
+	printf("usage: [insert(i) num | remove(r) num | infix | posfix | prefix | print(p) | clear | help | exit]\n");
 }
 
 int main(void) {
@@ -39,10 +39,10 @@ int main(void) {
             exit(0);
         }
 
-        if (strcmp(command, "insert") == 0) {
+        if (strcmp(command, "insert") == 0 || strcmp(command, "i") == 0) {
             scanf(" %d", &number);
             root = avl_insert(root, number);
-        } else if (strcmp(command, "remove") == 0) {
+        } else if (strcmp(command, "remove") == 0  || strcmp(command, "r") == 0) {
             scanf(" %d", &number);
             root = avl_remove(root, number);
         } else if (strcmp(command, "infix") == 0) {
@@ -51,7 +51,7 @@ int main(void) {
             avl_posfix(root);
         } else if (strcmp(command, "prefix") == 0) {
             avl_prefix(root);
-        } else if (strcmp(command, "print") == 0) {
+        } else if (strcmp(command, "print") == 0 || strcmp(command, "p") == 0) {
             print_ascii_tree(root);
         } else if (strcmp(command, "clear") == 0) {
             clear();
