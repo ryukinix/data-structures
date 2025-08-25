75aef465447af0e3f0b0d368a5677096d80aece6
Manoel Vilela <manoel.neto@neoway.com.br>
2025-08-24 21:08:15 -0300

    refactor: replace src/tree/main.c bst with avl implementation

--- src/tree/main.c
+++ src/tree/main.c
@@ -12,22 +12,22 @@
 
 #include <stdio.h>
 #include <string.h>
-#include "bst/bst.h"
+#include "avl/avl.h"
 #include "ascii-tree/ascii-tree.h"
 #include "console/console.h"
 
 /**
  * @description
- * A simple program to see what happens on BST
+ * A simple program to see what happens on AVL
  * interactively inserting and removing new elements
  */
- 
+
 void help() {
 	printf("usage: [insert num | remove num | infix | posfix | prefix | print | clear | help | exit]\n");
 }
 
 int main(void) {
-    BSTree *root = bst_create();
+    AVLTree *root = avl_create();
     int number;
     char command[50];
     help();
@@ -41,21 +41,21 @@ int main(void) {
 
         if (strcmp(command, "insert") == 0) {
             scanf(" %d", &number);
-            root = bst_insert(root, number);
+            root = avl_insert(root, number);
         } else if (strcmp(command, "remove") == 0) {
             scanf(" %d", &number);
-            root = bst_remove(root, number);
+            root = avl_remove(root, number);
         } else if (strcmp(command, "infix") == 0) {
-            bst_infix(root);
+            avl_infix(root);
         } else if (strcmp(command, "posfix") == 0) {
-            bst_posfix(root);
+            avl_posfix(root);
         } else if (strcmp(command, "prefix") == 0) {
-            bst_prefix(root);
+            avl_prefix(root);
         } else if (strcmp(command, "print") == 0) {
             print_ascii_tree(root);
         } else if (strcmp(command, "clear") == 0) {
             clear();
-        
+
         } else if (strcmp(command, "exit") == 0 || strcmp(command, "quit") == 0) {
             exit(0);
         } else {
