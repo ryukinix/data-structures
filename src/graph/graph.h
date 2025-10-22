2cd9b893077205982bfd18ce51f6c01cfce5813a
Manoel Vilela <manoel.neto@neoway.com.br>
2025-10-21 23:39:01 -0300

    docs: add docstrings in doxygen style

--- src/graph/graph.h
+++ src/graph/graph.h
@@ -18,14 +18,78 @@
 
 typedef struct Graph Graph;
 
+/**
+ * @brief Creates a new graph.
+ * @return A pointer to the new graph.
+ * @ingroup DataStructureMethods
+ */
 Graph* graph_create();
+
+/**
+ * @brief Adds a node to the graph.
+ * @param g The graph.
+ * @param node The node to be added.
+ * @ingroup DataStructureMethods
+ */
 void graph_add_node(Graph *g, int node);
+
+/**
+ * @brief Adds an edge to the graph.
+ * @param g The graph.
+ * @param u The source node.
+ * @param v The destination node.
+ * @ingroup DataStructureMethods
+ */
 void graph_add_edge(Graph *g, int u, int v);
+
+/**
+ * @brief Removes an edge from the graph.
+ * @param g The graph.
+ * @param u The source node.
+ * @param v The destination node.
+ * @ingroup DataStructureMethods
+ */
 void graph_remove_edge(Graph *g, int u, int v);
+
+/**
+ * @brief Removes a node from the graph.
+ * @param g The graph.
+ * @param node The node to be removed.
+ * @ingroup DataStructureMethods
+ */
 void graph_remove_node(Graph *g, int node);
+
+/**
+ * @brief Checks if an edge exists in the graph.
+ * @param g The graph.
+ * @param u The source node.
+ * @param v The destination node.
+ * @return True if the edge exists, false otherwise.
+ * @ingroup DataStructureMethods
+ */
 bool graph_has_edge(Graph *g, int u, int v);
+
+/**
+ * @brief Gets the neighbors of a node.
+ * @param g The graph.
+ * @param node The node.
+ * @return A set containing the neighbors of the node.
+ * @ingroup DataStructureMethods
+ */
 Set* graph_get_neighbors(Graph *g, int node);
+
+/**
+ * @brief Frees the memory allocated for the graph.
+ * @param g The graph.
+ * @ingroup DataStructureMethods
+ */
 void graph_free(Graph *g);
+
+/**
+ * @brief Prints the graph.
+ * @param g The graph.
+ * @ingroup DataStructureMethods
+ */
 void graph_print(Graph *g);
 
 #endif /* GRAPH_H */
