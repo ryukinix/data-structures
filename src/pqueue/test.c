553c2d179668693838ea2f22f2378ea911b8ddbd
Manoel V. Machado <manoel_vilela@engineer.com>
2025-10-28 18:29:53 -0300

    feat: priority queue of max/min generalized (#57)
    * feat: priority queue of max/min generalized
    
    * tests: add asserts
    
    * refactor: remove stdlib from tests.c, no need

--- src/pqueue/test.c
+++ src/pqueue/test.c
@@ -10,11 +10,14 @@
  * ===============================================
  */
 
-#include "pqueue.h"
 #include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
+#include "pqueue.h"
 
-void jarbas_test(void) {
-    PQueue *pq = pqueue_create();
+void test_max_pqueue(void) {
+    printf("--- Testing Max Priority Queue ---\n");
+    PQueue *pq = pqueue_create(MAX_PQUEUE);
 
     pqueue_insert(pq, 34);
     pqueue_insert(pq, 11);
@@ -23,27 +26,104 @@ void jarbas_test(void) {
     pqueue_insert(pq, 92);
     pqueue_insert(pq, 66);
 
-    printf("PQueue: ");
+    printf("Initial PQueue: ");
     pqueue_println(pq);
 
-    printf("Priority: %d\n", pqueue_maximum(pq));
-    int a = pqueue_extract_max(pq);
-    printf("PQueue: ");
+    printf("Top priority: %d\n", pqueue_top(pq));
+    int extracted = pqueue_extract(pq);
+    printf("Extracted: %d\n", extracted);
+    printf("PQueue after extract: ");
     pqueue_println(pq);
-    printf("Priorities: %d (extracted) and next %d\n", a, pqueue_maximum(pq));
-    a = pqueue_extract_max(pq);
-    printf("PQueue: ");
+
+    printf("Top priority: %d\n", pqueue_top(pq));
+    extracted = pqueue_extract(pq);
+    printf("Extracted: %d\n", extracted);
+    printf("PQueue after extract: ");
     pqueue_println(pq);
-    printf("Priorities: %d (extracted) and %d\n", a, pqueue_maximum(pq));
-    pqueue_increase_keys(pq, 2, 200);
-    printf("Increase index-2 element to 200 priority\n");
-    printf("Element of max priority: %d\n", pqueue_maximum(pq));
-    printf("PQueue: ");
+
+    pqueue_change_key(pq, 2, 200);
+    printf("Changed index-2 element to 200 priority\n");
+    printf("PQueue after change_key: ");
     pqueue_println(pq);
+    printf("Element of top priority: %d\n", pqueue_top(pq));
+
     pqueue_free(pq);
+    printf("--- End Max Priority Queue Test ---\n\n");
 }
 
+void test_min_pqueue(void) {
+    printf("--- Testing Min Priority Queue ---\n");
+    PQueue *pq = pqueue_create(MIN_PQUEUE);
+
+    printf("Inserting 10...\n");
+    pqueue_insert(pq, 10);
+    printf("PQueue: "); pqueue_println(pq);
+    assert(pqueue_size(pq) == 1);
+    assert(pqueue_top(pq) == 10);
+
+    printf("Inserting 20...\n");
+    pqueue_insert(pq, 20);
+    printf("PQueue: "); pqueue_println(pq);
+    assert(pqueue_size(pq) == 2);
+    assert(pqueue_top(pq) == 10);
+
+    printf("Inserting 5...\n");
+    pqueue_insert(pq, 5);
+    printf("PQueue: "); pqueue_println(pq);
+    assert(pqueue_size(pq) == 3);
+    assert(pqueue_top(pq) == 5);
+
+    printf("Inserting 25...\n");
+    pqueue_insert(pq, 25);
+    printf("PQueue: "); pqueue_println(pq);
+    assert(pqueue_size(pq) == 4);
+    assert(pqueue_top(pq) == 5);
+
+    printf("Inserting 15...\n");
+    pqueue_insert(pq, 15);
+    printf("PQueue: "); pqueue_println(pq);
+    assert(pqueue_size(pq) == 5);
+    assert(pqueue_top(pq) == 5);
+
+    printf("Size: %d\n", pqueue_size(pq));
+    printf("Min: %d\n", pqueue_top(pq));
+
+    printf("Extracting min...\n");
+    assert(pqueue_extract(pq) == 5);
+    printf("PQueue: "); pqueue_println(pq);
+    assert(pqueue_size(pq) == 4);
+    assert(pqueue_top(pq) == 10);
+
+    printf("Extracting min...\n");
+    assert(pqueue_extract(pq) == 10);
+    printf("PQueue: "); pqueue_println(pq);
+    assert(pqueue_size(pq) == 3);
+    assert(pqueue_top(pq) == 15);
+
+    printf("Extracting min...\n");
+    assert(pqueue_extract(pq) == 15);
+    printf("PQueue: "); pqueue_println(pq);
+    assert(pqueue_size(pq) == 2);
+    assert(pqueue_top(pq) == 20);
+
+    printf("Extracting min...\n");
+    assert(pqueue_extract(pq) == 20);
+    printf("PQueue: "); pqueue_println(pq);
+    assert(pqueue_size(pq) == 1);
+    assert(pqueue_top(pq) == 25);
+
+    printf("Extracting min...\n");
+    assert(pqueue_extract(pq) == 25);
+    printf("PQueue: "); pqueue_println(pq);
+    assert(pqueue_is_empty(pq));
+
+    pqueue_free(pq);
+    printf("--- End Min Priority Queue Test ---\n\n");
+}
+
+
 int main(void) {
-    jarbas_test();
+    test_max_pqueue();
+    test_min_pqueue();
     return 0;
 }
